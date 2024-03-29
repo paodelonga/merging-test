53faff8ff439b8eedd87e7db4458d1aa7d6a7a4c
paodelonga <72471292+paodelonga@users.noreply.github.com>
2024-03-29 06:07:20 -0300

    feat(lessons): update lesson

--- lessons/LS01/tasks/05/src/main.py
+++ lessons/LS01/tasks/05/src/main.py
@@ -10,6 +10,6 @@ Digite o numero em metros:<br>
 Em centímetros:<br>
 """
 
-metros = float(input("Digite um valor para converter em metros: "))
+metros = float(input("Digite um valor em metros para ser convertido em centimetros: "))
 cm = (metros * 100.00)
 print(f"{metros}m é equivalente a {cm:.2f}cm: ")
