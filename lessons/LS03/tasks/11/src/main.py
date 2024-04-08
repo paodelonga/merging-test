7e17aa5cf4406bec477eb0866c9cd7f1fb0aa8b8
paodelonga <72471292+paodelonga@users.noreply.github.com>
2024-04-08 04:38:06 -0300

    feat: check values to print order price

--- lessons/LS03/tasks/11/src/main.py
+++ lessons/LS03/tasks/11/src/main.py
@@ -19,19 +19,21 @@ Valor final:
 
 consumo_mesa = float(input("Qual valor de consumo na sua mesa? "))
 pessoas_mesa = int(input("Qual a quantidade de pessoas na sua mesa? "))
+
 valor_garcom = float()
 valor_conta = float()
 
-if(pessoas_mesa > 1):
-  valor_garcom = (5 * pessoas_mesa)
+if(pessoas_mesa > 0):
+  if(pessoas_mesa > 1):
+    valor_garcom = (5 * pessoas_mesa)
 
-if(consumo_mesa <= 49.99):
-  valor_garcom = ((10 * (consumo_mesa / 100)) + valor_garcom)
-  valor_conta = (consumo_mesa + valor_garcom)
-if(consumo_mesa >= 50 and consumo_mesa <= 99.99):
-  valor_garcom = ((5 * (consumo_mesa / 100)) + valor_garcom)
-  valor_conta = (consumo_mesa + valor_garcom)
-if(consumo_mesa >= 100):
-  valor_conta = (consumo_mesa + valor_garcom)
+  if(consumo_mesa <= 49.99):
+    valor_garcom = ((10 * (consumo_mesa / 100)) + valor_garcom)
+    valor_conta = (consumo_mesa + valor_garcom)
+  if(consumo_mesa >= 50 and consumo_mesa <= 99.99):
+    valor_garcom = ((5 * (consumo_mesa / 100)) + valor_garcom)
+    valor_conta = (consumo_mesa + valor_garcom)
+  if(consumo_mesa >= 100):
+    valor_conta = (consumo_mesa + valor_garcom)
 
-print(f"Para a quantidade de {pessoas_mesa} pessoas o valor total da conta foi de {valor_conta:.2f} incluindo o adicional de {valor_garcom:.2f} para o Garcom.")
+  print(f"Para a quantidade de {pessoas_mesa} pessoas o valor total da conta foi de {valor_conta:.2f} incluindo o adicional de {valor_garcom:.2f} para o Garcom.")
